1e7c913841c0d90b708a98a64b120830ca63e435
qianhk <qhk83@qq.com>
2015-07-08 11:26:53 +0800

    test show select date value.

--- sample/src/com/squareup/timessquare/sample/SingleMonthActivity.java
+++ sample/src/com/squareup/timessquare/sample/SingleMonthActivity.java
@@ -2,10 +2,13 @@ package com.squareup.timessquare.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import com.squareup.timessquare.CalendarView;
 import com.squareup.timessquare.Logr;
 import com.squareup.timessquare.MonthCellDescriptor;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -46,9 +49,21 @@ public class SingleMonthActivity extends Activity {
         super.onCreate(savedInstanceState);
         Logr.d("lookMonth onCreate");
         setContentView(R.layout.single_month_page);
-        CalendarView monthView = (CalendarView) findViewById(R.id.month_view);
+        final CalendarView monthView = (CalendarView) findViewById(R.id.month_view);
         Logr.d("lookMonth findViewById month_view");
         monthView.setDate(new Date(2015 - 1900, 6, 20));
+        final TextView tvNowDate = (TextView) findViewById(R.id.tv_now_date);
+        findViewById(R.id.btn_now_date).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Date selectedDate = monthView.getSelectedDate();
+                final Calendar calendar = Calendar.getInstance();
+                calendar.setTime(selectedDate);
+                String dateStr = String.format("  %04d-%02d-%02d"
+                        , calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
+                tvNowDate.setText(dateStr);
+            }
+        });
     }
 
 
